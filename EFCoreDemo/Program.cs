bdd7d2e5109d51504e1fa585984760e098711fd3
Artur Hoffman <thearturoo87@gmail.com>
2018-08-23 21:25:55 +0200

    Add project files.

---
+++ EFCoreDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EFCoreDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+
+                var std = new Student()
+                {
+                    Name = "Bill"
+                };
+
+                context.Students.Add(std);
+                context.SaveChanges();
+            }
+        }
+
+        public class Student
+        {
+            public int StudentId { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Course
+        {
+            public int CourseId { get; set; }
+            public string CourseName { get; set; }
+        }
+
+        public class SchoolContext : DbContext
+        {
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Course> Courses { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                //optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Data Source=ARTUROO-PC;Initial Catalog=SchoolDB;Integrated Security=True;Pooling=False");
+                //Data Source=ARTUROO-PC;Initial Catalog=SchoolDB;Integrated Security=True;Pooling=False
+            }
+        }
+
+    }
+}
